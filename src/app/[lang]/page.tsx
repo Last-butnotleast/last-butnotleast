13ded6e749ac34b48ad55925a7064e085cdb44b0
Tom Last <tom@elbtech.dev>
2024-12-18 11:57:42 +0100

    multi language setup

---
+++ src/app/[lang]/page.tsx
@@ -0,0 +1,20 @@
+import { getDictionary } from "@/get-dictionary";
+import { Locale } from "@/i18n-config";
+
+export default async function Home({
+  params,
+}: {
+  params: Promise<{ lang: Locale }>;
+}) {
+  const lang = (await params).lang;
+  const dictionary = await getDictionary(lang);
+
+  return (
+    <main className="flex flex-col items-center justify-center h-screen">
+      <p>
+        Current locale: <b>{lang}</b>
+      </p>
+      <h1>{dictionary["server-component"].welcome}</h1>
+    </main>
+  );
+}
